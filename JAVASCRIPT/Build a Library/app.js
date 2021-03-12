d712874eb40e17e0d9f4512881834d7e9e192528
Stephanie Chen <70101905+chenmeijing@users.noreply.github.com>
2021-03-12 17:10:50 +0630

    Add files via upload
    Books-‘N-Stuff carries three different types of media: books, CDs, and movies. In this project I create a parent class named Media with three subclasses: Book, Movie, and CD. The three subclasses have their properties and methods In this project, I have used the knowledge of JavaScript's Class  which is a template literal of Object.

---
+++ JAVASCRIPT/Build a Library/app.js
@@ -0,0 +1,102 @@
+class Media {
+  constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  get title(){
+    return this._title;
+  }
+  get isCheckedOut(){
+    return this._isCheckedOut;
+  }
+  get ratings(){
+    return this._ratings;
+  }
+  set isCheckedOut(newCheck){
+    this._isCheckedOut = newCheck;
+  }
+
+  toggleCheckOutStatus(){
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+  
+   addRating(inputValue){
+    this.ratings.push(inputValue);
+
+  }
+
+  getAverageRating(){
+   let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating);
+  return ratingsSum / this.ratings.length;
+  }
+
+ 
+}
+
+class Book extends Media{
+  constructor(author,title, pages){
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author(){
+    return this._author;
+  }
+  get pages(){
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(director,title,runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director(){
+    return this._director;
+  }
+  get runTime(){
+    return this._runTime;
+  }
+}
+
+class CD extends Media{
+  constructor(artist, title, songs){
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+  get artist(){
+    return this._artist;
+  }
+  
+  get songs(){
+    return this._songs;
+  }
+  shuffle(){
+    return this.songs[Math.floor(Math.random() * this.songs.length)];
+  }
+
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4,5,6);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1,1,5);
+console.log(speed.getAverageRating());
+
+const album = new CD('Taylor Swift', 'folklore', ['Cardigan', 'Exile', 'Seven', 'August', 'Invisible String', 'Epiphany', 'Hoax', 'The Lakes', 'This is Me Trying', 'Illicit Affairs', 'The One']);
+
+album.toggleCheckOutStatus();
+console.log(album.isCheckedOut);
+album.addRating(4,3,5);
+console.log(album.getAverageRating());
+console.log(album.shuffle());
